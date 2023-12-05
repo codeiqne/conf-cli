9a713a38fc24e9a28d44c1ade4a58deed2283a1d
codeiqne <this-email-does-not-exist@codeiqne.xyz>
2023-12-05 17:46:44 +0100

    upgraded deps, tests passing

--- src/commands/conf.ts
+++ src/commands/conf.ts
@@ -1,21 +1,21 @@
 import {Command, flags} from '@oclif/command'
 import * as Conf from 'conf'
-import * as os from 'os'
+import * as os from 'node:os'
 
 export default class ConfCommand extends Command {
+  static args = [
+    {description: 'key of the config', name: 'key'},
+    {description: 'value of the config', name: 'value'}
+  ]
+
   static description = 'manage configuration'
 
   static flags = {
+    delete: flags.boolean({char: 'd', description: 'delete?'}),
     help: flags.help({char: 'h'}),
     key: flags.string({char: 'k', description: 'key of the config'}),
-    value: flags.string({char: 'v', description: 'value of the config'}),
-    delete: flags.boolean({char: 'd', description: 'delete?'})
+    value: flags.string({char: 'v', description: 'value of the config'})
   }
-
-  static args = [
-    {name: 'key', description: 'key of the config'},
-    {name: 'value', description: 'value of the config'}
-  ]
 
   async run() {
     const {args, flags} = this.parse(ConfCommand)
@@ -37,16 +37,18 @@ export default class ConfCommand extends Command {
         }
       }
     } else {
-      for (let c of config) {
+      for (const c of config) {
         this.print(c[0] + os.EOL)
       }
     }
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private print(value: any) {
     if (typeof value === 'object') {
       value = JSON.stringify(value)
     }
+
     process.stdout.write(value)
   }
 }
